3d1f7e0df87945b61621f6ced28bcdcc5a82c957
patricio.stickar <patricio.stickar@moveax.it>
2024-01-18 19:11:58 +0800

    5.1: Keyboard users

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,20 @@ export const ToastContext = React.createContext(null);
 function ToastProvider({children}) {
 	const [toasts, setToasts] = React.useState([]);
 
+	React.useEffect(() => {
+		function handleKeyDown(event) {
+			if (event.code === 'Escape') {
+				setToasts([]);
+			}
+		}
+
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, []);
+
 	function createToast(message, variant){
 		setToasts([...toasts, {message, variant, id: crypto.randomUUID()}]);
 	}
